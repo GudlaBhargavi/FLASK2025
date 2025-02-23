46a2b2b7f976500baa0bf591430df5e36c572eec
Bhargavi-student <bhargavi8260@gmail.com>
2025-02-23 19:03:41 +0530

    added nav bar and email

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello_world():
+    return render_template('index.html')
     return "Hello, Bhargavi! Welcome to Flask."
 
+@app.route('/products')
+def products():
+    return 'this is products page'
 if __name__ == '__main__':
     app.run(debug=True)
