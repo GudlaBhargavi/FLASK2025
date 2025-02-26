023ca2ae7168f3ec181ae721c389015c8653c505
Bhargavi-student <bhargavi8260@gmail.com>
2025-02-26 15:38:50 +0530

    addedfunctionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -41,11 +41,12 @@ def update():
     print(allTodo)
     return 'This is the products page'
 
-@app.route('/delete')
-def delete():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'This is the products page'
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == '__main__':
